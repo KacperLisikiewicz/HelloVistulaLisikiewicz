b3a378fabe1415c196fb584c46a2d035a2b3711a
User <you@example.com>
2022-10-07 13:43:58 +0200

    Dodanie pare zdań

--- src/pl/vistula/helloworldlisikiewicz/HelloWorldLisikiewicz.java
+++ src/pl/vistula/helloworldlisikiewicz/HelloWorldLisikiewicz.java
@@ -3,5 +3,10 @@ package pl.vistula.helloworldlisikiewicz;
 public class HelloWorldLisikiewicz {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Lisikiewicz");
+        System.out.println("nie wiem co dalej");
+        System.out.println("nie wiem co dalej nadal");
+        System.out.println("nie wiem co dalej całkowicie");
+
+
     }
 }
