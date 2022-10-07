f591399d545bb1e95e74faafc2be12a6fc538c9d
User <you@example.com>
2022-10-07 14:23:42 +0200

    usuwanie commit

--- src/pl/vistula/helloworldlisikiewicz/HelloWorldLisikiewicz.java
+++ src/pl/vistula/helloworldlisikiewicz/HelloWorldLisikiewicz.java
@@ -4,7 +4,7 @@ public class HelloWorldLisikiewicz {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Lisikiewicz");
         System.out.println("nie wiem co dalej");
-        System.out.println("nie wiem co dalej nadal");
+
         System.out.println("nie wiem co dalej całkowicie");
 
 
