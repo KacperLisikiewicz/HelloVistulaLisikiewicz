cfdc7900d80c0172c63de360c72196c241f0f045
User <you@example.com>
2022-10-07 13:38:54 +0200

    Initial commit

---
+++ src/pl/vistula/helloworldlisikiewicz/HelloWorldLisikiewicz.java
@@ -0,0 +1,7 @@
+package pl.vistula.helloworldlisikiewicz;
+
+public class HelloWorldLisikiewicz {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula Lisikiewicz");
+    }
+}
